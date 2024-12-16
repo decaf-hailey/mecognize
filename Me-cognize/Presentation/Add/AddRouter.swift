17c1d9ffaa930ed4519b608810713ec566fd6eeb
Hailey Kwak <124033750+decaf-hailey@users.noreply.github.com>
2024-12-16 14:17:43 -0800

    remove useless code

--- Me-cognize/Presentation/Add/AddRouter.swift
+++ Me-cognize/Presentation/Add/AddRouter.swift
@@ -23,35 +23,4 @@ protocol AddDataPassing {
 class AddRouter: NSObject, AddRoutingLogic, AddDataPassing {
   weak var viewController: AddViewController?
   var dataStore: AddDataStore?
-  
-  // MARK: Routing
-  
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
-  //{
-  //  if let segue = segue {
-  //    let destinationVC = segue.destination as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //  } else {
-  //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-  //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-  //  }
-  //}
-
-  // MARK: Navigation
-  
-  //func navigateToSomewhere(source: AddViewController, destination: SomewhereViewController)
-  //{
-  //  source.show(destination, sender: nil)
-  //}
-  
-  // MARK: Passing data
-  
-  //func passDataToSomewhere(source: AddDataStore, destination: inout SomewhereDataStore)
-  //{
-  //  destination.name = source.name
-  //}
 }
